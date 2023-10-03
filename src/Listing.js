b8a859f1097dcc63588cf76bb3318bda6b28797c
Oleg Khvostenko <xboctehko@yandex.ru>
2023-10-03 12:54:43 +0200

    Keys added

--- src/Listing.js
+++ src/Listing.js
@@ -3,31 +3,26 @@
 const currency = {'USD': '$', 'EUR': '€'};
 const levels = ['level-low', 'level-medium', 'level-high'];
 
-function Card(item) {
-  if (item.state === "active") {
-    return (
-      <div className="item">
-        <div className="item-image">
-          <a href={item.url}>
-            <img src={item.MainImage["url_570xN"]} alt="..."/>
-          </a>
-        </div>
-        <div className="item-details">
-          <p className="item-title">{item.title.length <= 50? item.title : item.title.slice(0,50) + "…"}</p>
-          <p className="item-price">
-            {Object.keys(currency).includes(item.currency_code)? currency[item.currency_code] + item.price : item.price + " " + item.currency_code}
-          </p>
-          <p className={"item-quantity " + levels[Math.min(Math.floor((item.quantity-1)/10),2)]}>{item.quantity + " left"}</p>
-        </div>
-      </div>
-    );
-  }
-}
-
 export default function Listing({items}) {
   return (
     <div className="item-list">
-      { items.map(item => <>{Card(item)}</>) }
+      {items.filter(item => item.state === "active").map(item =>
+        <div key={item.listing_id} className="item">
+          <div className="item-image">
+            <a href={item.url}>
+              <img src={item.MainImage.url_570xN} alt="..."/>
+            </a>
+          </div>
+          <div className="item-details">
+            <p className="item-title">{item.title.length <= 50? item.title : item.title.slice(0,50) + "…"}</p>
+            <p className="item-price">
+              {Object.keys(currency).includes(item.currency_code)? 
+                currency[item.currency_code] + item.price : item.price + " " + item.currency_code}
+            </p>
+            <p className={"item-quantity " + levels[Math.min(Math.floor((item.quantity-1)/10),2)]}>{item.quantity} left</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
